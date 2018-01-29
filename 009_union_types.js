9a6c8c6807b63280422c85571d2cf8343e20a071
Jerel Lillywhite <supelillywhite@gmail.com>
2018-01-29 15:38:40 -0700

    Union types example

---
+++ 009_union_types.js
@@ -0,0 +1,10 @@
+var playersTwo = ["Altuve", "Corerra", "Bregman"];
+var playerNumbers = [7, 4, 8];
+console.log(playersTwo);
+console.log(playerNumbers);
+var names;
+names = ["Bill", "Ted"];
+console.log(names);
+names = "Supe";
+console.log(names);
+//# sourceMappingURL=009_union_types.js.map
---
+++ 009_union_types.ts
@@ -0,0 +1,11 @@
+type PlayerArrayTwo = Array<string|number>;
+let playersTwo : PlayerArrayTwo = ["Altuve", "Corerra", "Bregman"];
+let playerNumbers : PlayerArrayTwo = [7, 4, 8];
+console.log(playersTwo);
+console.log(playerNumbers);
+
+var names : string[]|string;
+names = ["Bill", "Ted"];
+console.log(names);
+names = "Supe";
+console.log(names);
