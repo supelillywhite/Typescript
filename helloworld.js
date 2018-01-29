229d10a1e9a3e891e3e0a342b9c8263d93854726
Jerel Lillywhite <supelillywhite@gmail.com>
2018-01-29 13:26:36 -0700

    Initial commit for typescript files

---
+++ helloworld.js
@@ -0,0 +1,6 @@
+var HelloAngularComponent = /** @class */ (function () {
+    function HelloAngularComponent() {
+        this.greeting = "Hello Angular 2";
+    }
+    return HelloAngularComponent;
+}());
